9d35c2c2ca2245cbcc6191c6fe8819b16b908cd7
lstarono <lstarono@redhat.com>
2025-07-09 15:15:53 +0200

    Fix: styling changes in Header comp.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Content } from '@patternfly/react-core';
+import { Content, Flex } from '@patternfly/react-core';
 import {
   OpenSourceBadge,
   PageHeader as _PageHeader,
@@ -7,23 +7,12 @@ import {
 import { PageHeaderProps as _PageHeaderProps } from '@redhat-cloud-services/frontend-components/PageHeader/PageHeader';
 
 import { FunctionComponent, ReactElement } from 'react';
-import { createUseStyles } from 'react-jss';
 
 interface PageHeaderProps extends _PageHeaderProps {
   children?: ReactElement | Array<ReactElement>;
 }
 
-// Example of how to extend inaccurately typed imports
 const PageHeader = _PageHeader as FunctionComponent<PageHeaderProps>;
-
-const useStyles = createUseStyles({
-  subtext: {
-    paddingTop: '8px',
-  },
-  remove100percent: {
-    height: 'unset',
-  },
-});
 
 interface Props {
   title: string;
@@ -32,22 +21,24 @@ interface Props {
 }
 
 export default function Header({ title, ouiaId, paragraph }: Props) {
-  const classes = useStyles();
-
   return (
     <PageHeader>
-      <PageHeaderTitle
-        title={
-          <>
-            {title}
-            <OpenSourceBadge repositoriesURL='https://github.com/content-services/content-sources-frontend' />
-          </>
-        }
-        className={classes.remove100percent}
-      />
-      <Content component='p' className={classes.subtext} ouiaId={ouiaId}>
-        {paragraph}
-      </Content>
+      <Flex className='pf-v6-u-m-xs pf-v6-u-pb-sm' direction={{ default: 'column' }}>
+        <PageHeaderTitle
+          title={
+            <Flex
+              alignItems={{ default: 'alignItemsCenter' }}
+              columnGap={{ default: 'columnGapNone' }}
+            >
+              <span>{title}</span>
+              <OpenSourceBadge repositoriesURL='https://github.com/content-services/content-sources-frontend' />
+            </Flex>
+          }
+        />
+        <Content component='p' ouiaId={ouiaId}>
+          {paragraph}
+        </Content>
+      </Flex>
     </PageHeader>
   );
 }
