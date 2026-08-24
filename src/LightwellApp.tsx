a5c46c0e0bc71c8635be96d167943ebc1defbfa8
Bryttanie House <bhouse@redhat.com>
2026-08-24 12:23:27 -0400

    fix: remove beacon from demo route

--- src/LightwellApp.tsx
+++ src/LightwellApp.tsx
@@ -35,7 +35,6 @@ export default function LightwellApp() {
         <Routes>
           <Route path='demo' element={<LightwellDemoLayout />}>
             <Route index element={<RepositoriesTable />} />
-            <Route path='beacon' element={<Beacon />} />
             <Route path=':repoName/:group/:packageName' element={<PackageDetails />} />
             <Route path=':repoName/:packageName' element={<PackageDetails />} />
             <Route path=':repoName' element={<PackagesTable />} />
